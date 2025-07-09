f9edca69aeff3b07b1c5227a8205caa2a7e2bd6e
Biswajit Malakar <mebiswajitmalakar@gmail.com>
2025-07-09 22:17:21 +0530

    feat: integrate swagger UI for api doc only the setup part is done

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,17 @@
+import swaggerUI from "swagger-ui-express";
+import yaml from "yaml";
+import fs from "fs";
+import path from "path";
 import app from "./app";
 import env from "@environment";
 
+const specPath = path.resolve(__dirname, "../openapi/spec.yaml");
+const file = fs.readFileSync(specPath, "utf8");
+const openapiSpec = yaml.parse(file);
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(openapiSpec));
+
 app.listen(env.PORT, () => {
   console.log(`Zora API server is listening on ${env.HOST}/${env.PORT}}`);
-})
+  console.log(`Zora internal API documentation is listening on ${env.HOST}/${env.PORT}/docs`)
+});
