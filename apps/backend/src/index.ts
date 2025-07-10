c88af49bab4a9edc8517d3c63bcbf06eb1de27f3
Biswajit Malakar <mebiswajitmalakar@gmail.com>
2025-07-10 22:55:49 +0530

    Init logger use winston for logger for api server init basic logger log stored in local device

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,15 +3,16 @@ import yaml from "yaml";
 import fs from "fs";
 import path from "path";
 import app from "./app";
-import env from "@environment";
+import environment from "@environment";
+import logger from "@logger";
 
 const specPath = path.resolve(__dirname, "../openapi/spec.yaml");
 const file = fs.readFileSync(specPath, "utf8");
 const openapiSpec = yaml.parse(file);
 
-app.use("/docs", swaggerUI.serve, swaggerUI.setup(openapiSpec));
+app.use("/internal-docs", swaggerUI.serve, swaggerUI.setup(openapiSpec));
 
-app.listen(env.PORT, () => {
-  console.log(`Zora API server is listening on ${env.HOST}/${env.PORT}}`);
-  console.log(`Zora internal API documentation is listening on ${env.HOST}/${env.PORT}/docs`)
+app.listen(environment.PORT, () => {
+  logger.info(`Zora API server is listening on ${environment.NODE_ENV === "development" ? "http" : "https"}://${environment.HOST}/${environment.PORT}`);
+  logger.info(`Zora internal API reference is listening on ${environment.NODE_ENV === "development" ? "http" : "https"}://${environment.HOST}/${environment.PORT}/internal-docs`)
 });
