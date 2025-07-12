2c39e089279789c0550b46a42f662a551bc79b17
Biswajit Malakar <mebiswajitmalakar@gmail.com>
2025-07-12 20:47:53 +0530

    Init morgan Init basic config for morgan a http logger middleware

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,5 +1,11 @@
 import express, { Express } from "express";
+import morgan from "@morgan";
+import environment from "@environment";
 
 const app: Express = express();
+
+if (environment.NODE_ENV != "production") {
+  app.use(morgan);
+}
 
 export default app;
